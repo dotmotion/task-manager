db6061ad6f84b3e7ff76bf89197020a38a252678
Andres Outon <outon.x@gmail.com>
2018-10-12 16:16:53 -0500

    added context handler for state and actions

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,39 +1,47 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Consumer } from "../context";
 
 class Task extends Component {
-  deleteItem = () => {
-    this.props.deleteHandler();
+  deleteItem = (id, dispatch) => {
+    dispatch({ type: "DELETE_TASK", payload: id });
   };
+
   render() {
-    const { name, desc, time, status } = this.props.task;
+    const { id, name, desc, time, status } = this.props.task;
 
     return (
-      <div className="card card-body mb-3 bg-secondary shadow p-3 mb-5 mr-5 bg-white rounded">
-        <h4>
-          {name}
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3 bg-secondary shadow p-3 mb-5 mr-5 bg-white rounded">
+              <h4>
+                {name}
 
-          <i
-            className="fas fa-times"
-            style={{ cursor: "pointer", color: "red", float: "right" }}
-            onClick={this.deleteItem}
-          />
-        </h4>
+                <i
+                  className="fas fa-times"
+                  style={{ cursor: "pointer", color: "red", float: "right" }}
+                  onClick={this.deleteItem.bind(this, id, dispatch)}
+                />
+              </h4>
 
-        <h6>{time}</h6>
+              <h6>{time}</h6>
 
-        <ul className="list-group">
-          <li className="list-group-item">{desc}</li>
-          <li className="list-group-item">Status: {status}</li>
-        </ul>
-      </div>
+              <ul className="list-group">
+                <li className="list-group-item">{desc}</li>
+                <li className="list-group-item">Status: {status}</li>
+              </ul>
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
 
 Task.propTypes = {
-  task: PropTypes.object.isRequired,
-  deleteHandler: PropTypes.func.isRequired
+  task: PropTypes.object.isRequired
 };
 
 export default Task;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,55 +1,22 @@
 import React, { Component } from "react";
 import Task from "./Task";
+import { Consumer } from "../context";
 
 class Tasks extends Component {
-  state = {
-    tasks: [
-      {
-        id: 1,
-        name: "Build Task Manager",
-        desc: "React Application",
-        time: "01:30",
-        status: "Work in Progress"
-      },
-      {
-        id: 2,
-        name: "Build Portfolio",
-        desc: "Static Website with SASS",
-        time: "01:00",
-        status: "Completed"
-      },
-      {
-        id: 3,
-        name: "Build Budget Manager",
-        desc: "Vanilla JS Budget Application",
-        time: "00:45",
-        status: "Completed"
-      }
-    ]
-  };
-
-  deleteTask = id => {
-    const { tasks } = this.state;
-
-    const newTasks = tasks.filter(task => task.id !== id);
-
-    this.setState({
-      tasks: newTasks
-    });
-  };
-
   render() {
-    const { tasks } = this.state;
     return (
-      <React.Fragment>
-        {tasks.map(task => (
-          <Task
-            key={task.id}
-            task={task}
-            deleteHandler={this.deleteTask.bind(this, task.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { tasks } = value;
+          return (
+            <React.Fragment>
+              {tasks.map(task => (
+                <Task key={task.id} task={task} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
