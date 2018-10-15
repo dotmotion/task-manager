cfa342cc931fb356a8298c3811b56102eacbaf44
Andres Outon <outon.x@gmail.com>
2018-10-15 12:59:05 -0500

    Updates

---
+++ precache-manifest.33db1dbf746af543e97bec24f6083833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6791f057bc95d4fb2055",
+    "url": "/static/js/main.6791f057.chunk.js"
+  },
+  {
+    "revision": "30ce4c37ab5107b15133",
+    "url": "/static/js/1.30ce4c37.chunk.js"
+  },
+  {
+    "revision": "6791f057bc95d4fb2055",
+    "url": "/static/css/main.b9abf2ef.chunk.css"
+  },
+  {
+    "revision": "30ce4c37ab5107b15133",
+    "url": "/static/css/1.671c63d8.chunk.css"
+  },
+  {
+    "revision": "2065015e26ce476ff77bf42c7f8804c5",
+    "url": "/index.html"
+  }
+];
