d545776b24cf708296dc5eb2b0418a8374349343
Andres Outon <outon.x@gmail.com>
2018-10-18 03:58:56 -0500

    added pages for routing

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="display-4">About Task Manager</h1>
+      <p className="lead">Simple Task Manager Application</p>
+      <p className="text-secondary">v1.0</p>
+    </div>
+  );
+}
