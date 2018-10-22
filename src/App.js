1f3b037d0193a15e0283da6459e86e7246688c40
Andres Outon <outon.x@gmail.com>
2018-10-22 18:42:47 -0500

    changed router config

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Tasks from "./components/tasks/Tasks";
 import AddTask from "./components/tasks/AddTask";
 import Header from "./components/layout/Header";
 import About from "./components/pages/About";
-import NotFound from "./components/pages/NotFound";
+// import NotFound from "./components/pages/NotFound";
 import { Provider } from "./context";
 import "./App.css";
 
@@ -13,15 +13,15 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <Router>
+        <Router basename="/task-manager/">
           <div className="App">
             <Header branding="Task Manager" />
             <div className="container d-flex flex-column justify-content-center align-items-center">
               <Switch>
                 <Route exact path="/" component={Tasks} />
-                <Route exact path="/task/add" component={AddTask} />
+                <Route exact path="/add" component={AddTask} />
                 <Route exact path="/about" component={About} />
-                <Route component={NotFound} />
+                <Route component={Tasks} />
               </Switch>
             </div>
           </div>
