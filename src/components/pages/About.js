29b0547cf6b38ad4ec21701e2c47136271f9cc50
Andres Outon <outon.x@gmail.com>
2018-10-23 13:45:47 -0500

    updated about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,6 +5,13 @@ export default function About() {
     <div>
       <h1 className="display-4">About Task Manager</h1>
       <p className="lead">Simple Task Manager Application</p>
+      <p className="lead">
+        So far you can Add and Delete Tasks, as well as start and stop each
+        Task's timer
+      </p>
+      <p className="lead">
+        When adding a task, you can only select 3 durations
+      </p>
       <p className="text-secondary">v1.0</p>
     </div>
   );
