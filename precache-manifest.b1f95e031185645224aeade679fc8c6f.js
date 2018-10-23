fd335740faef94a3f8d39a116db60ff60db006c1
Andres Outon <outon.x@gmail.com>
2018-10-22 20:29:59 -0500

    Updates

---
+++ precache-manifest.b1f95e031185645224aeade679fc8c6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02ab4459c7bcab350760",
+    "url": "/task-manager/static/js/runtime~main.02ab4459.js"
+  },
+  {
+    "revision": "e01004191c3c0a123c44",
+    "url": "/task-manager/static/js/main.e0100419.chunk.js"
+  },
+  {
+    "revision": "26be853d4095ec74a52b",
+    "url": "/task-manager/static/js/1.26be853d.chunk.js"
+  },
+  {
+    "revision": "e01004191c3c0a123c44",
+    "url": "/task-manager/static/css/main.35319555.chunk.css"
+  },
+  {
+    "revision": "26be853d4095ec74a52b",
+    "url": "/task-manager/static/css/1.ad74a0f2.chunk.css"
+  },
+  {
+    "revision": "5fb80a0add859fd93fd1bb8137a0527e",
+    "url": "/task-manager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/task-manager/precache-manifest.715896f48ea13d8793c4f875a131dd53.js"
+  "/task-manager/precache-manifest.b1f95e031185645224aeade679fc8c6f.js"
 );
 
 workbox.clientsClaim();
